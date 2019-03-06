fe46e2c80c3976d8c4521f358e41d1c126e4e4f0
Your Name <you@example.com>
2019-03-06 20:01:36 +0600

    my first commit

---
+++ Js/script.js
@@ -0,0 +1,7 @@
+document.body.onclick = function (event){
+    if(event target.classlist.contains('items-min'))
+        document.getElementById('item-max')src=event.target.src;
+    let allDiv = document.querySelectorAll('.item-img-min div');
+    console.log(allDiv);
+    event.target.parentElement
+}
